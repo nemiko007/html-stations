73821583fb9fe647b7d60b3299f9614b5587429c
nemiko007 <otoha.youjopero2@gmail.com>
2025-12-06 08:34:20 +0900

    a

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -11,6 +11,17 @@ async function getData() {
 }
 
 function test() {
-  return
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const userList = [
+        { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
+        { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+      ];
+      const fullNameList = userList.map(user => ({
+        full_name: user.family_name + ' ' + user.first_name
+      }));
+      resolve(fullNameList);
+    }, 3000);
+  })
 }
 
